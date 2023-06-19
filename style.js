b3d0daacc946c3f4b47e7a83673a08bf2988b24c
Yakub Yusuf <yakubyusuf608@gmail.com>
2023-06-19 14:46:35 +0100

    commit

---
+++ style.js
@@ -0,0 +1,45 @@
+const home = ()=>{
+    window.location.href = "index.html"
+}
+const ourEffort = ()=>{
+    window.location.href = "ourEffort.html"
+}
+const getInvolved = ()=>{
+    window.location.href = "getInvolved.html"
+}
+const aboutUs = ()=>{
+    window.location.href = "aboutUs.html"
+}
+const contactUs = ()=>{
+    window.location.href = "contactUs.html"
+}
+const donate = ()=>{
+    window.location.href = "donate.html"
+}
+const theProblem = ()=>{
+    window.location.href = "problems.html"
+}
+const theSolution = ()=>{
+    window.location.href = "solutions.html"
+}
+const waysGive = ()=>{
+    window.location.href = "ways.html"
+}
+const fund = ()=>{
+    window.location.href = "fundraise.html"
+}
+const fundCamp = ()=>{
+    window.location.href = "fundraiseCam.html"
+}
+const faqs = ()=>{
+    window.location.href = "faqs.html"
+}
+const btnF = ()=>{
+    window.location.href = "donate.html"
+}
+const invBtn = ()=>{
+    window.location.href = "fundraiseCam.html"
+}
+const way = ()=>{
+    window.location.href = "ways.html"
+}
